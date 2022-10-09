7c236efda6333768e531eed91f1ffb6d8f82ae06
Mario Ravalli <mario.ravalli@bakeca.it>
2022-10-09 17:37:27 +0200

    Rewrited form

--- lib/page/Home.dart
+++ lib/page/Home.dart
@@ -1,8 +1,12 @@
 import 'package:cbl/cbl.dart';
 import 'package:flutter/material.dart';
-import '../customer/Customer.dart';
-import '../customer/CustomerRepository.dart';
-import '../db/DatabaseHelper.dart';
+import 'package:gestio/document/EngagementRepository.dart';
+import 'package:gestio/document/machine/MachineRepository.dart';
+import 'package:gestio/document/customer/Customer.dart';
+import 'package:gestio/document/customer/CustomerRepository.dart';
+import 'package:gestio/db/DatabaseHelper.dart';
+import 'package:gestio/document/machine/Machine.dart';
+import 'package:intl/intl.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -14,24 +18,48 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   int _currentSortColumn = 0;
   bool _isSortAsc = true;
-  late List<Customer> _customers;
+  late List<Machine> _machines;
+
   late CustomerRepository _customerRepository;
+  late MachineRepository _machineRepository;
+
   final TextEditingController _firstnameController = TextEditingController();
   final TextEditingController _lastnameController = TextEditingController();
   final TextEditingController _addressController = TextEditingController();
+  final TextEditingController _phoneController = TextEditingController();
+
+  final TextEditingController _modelController = TextEditingController();
+  final TextEditingController _fluelController = TextEditingController();
+  final TextEditingController _numberController = TextEditingController();
+  final TextEditingController _registeredCodeController = TextEditingController();
+
+  final TextEditingController _lastDeadlineController = TextEditingController();
+  final TextEditingController _lastMarkController = TextEditingController();
+  final TextEditingController _deadlineController = TextEditingController();
+
+  DateTime? _lastDeadline = null;
+  DateTime? _lastMark = null;
+  DateTime? _deadline = null;
 
   // This function will be triggered when the floating button is pressed
   // It will also be triggered when you want to update an item
   void _showForm(String? id) async {
-    if (id != null) {
-      // id == null -> create new item
-      // id != null -> update an existing item
-      final existingCustomer =
-      _customers.firstWhere((element) => element.id == id);
-      _firstnameController.text = existingCustomer.firstname;
-      _lastnameController.text = existingCustomer.lastname;
-      _addressController.text = existingCustomer.address;
-    }
+    // if (id != null) {
+    //   // id == null -> create new item
+    //   // id != null -> update an existing item
+    //   final existingMachine = _machines.firstWhere((element) => element.id == id);
+    //   final existingCustomer =
+    //   _customers.firstWhere((element) => element.id == id);
+    //   _firstnameController.text = existingCustomer.firstname;
+    //   _lastnameController.text = existingCustomer.lastname;
+    //   _addressController.text = existingCustomer.address;
+    //   _phoneController.text = existingCustomer.phone ?? '';
+    // }
+
+    final EngagementRepository engagementRepository = EngagementRepository();
+    engagementRepository.allDocumentStream(new DateTime(2020), new DateTime(2023));
+
+    _machineRepository = MachineRepository(DatabaseHelper.instance.database!);
 
     showModalBottomSheet(
         context: context,
@@ -49,23 +77,152 @@ class _HomePageState extends State<HomePage> {
             mainAxisSize: MainAxisSize.min,
             crossAxisAlignment: CrossAxisAlignment.end,
             children: [
-              TextField(
-                controller: _firstnameController,
-                decoration: const InputDecoration(hintText: 'Firstname'),
+              Row(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Flexible(child: TextField(
+                    controller: _firstnameController,
+                    decoration: const InputDecoration(hintText: 'Firstname'),
+                  )),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  Flexible(child: TextField(
+                    controller: _lastnameController,
+                    decoration: const InputDecoration(hintText: 'Lastname'),
+                  ))
+                ],
               ),
               const SizedBox(
                 height: 10,
               ),
-              TextField(
-                controller: _lastnameController,
-                decoration: const InputDecoration(hintText: 'Lastname'),
+              Row(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Flexible(child: TextField(
+                    controller: _addressController,
+                    decoration: const InputDecoration(hintText: 'Address'),
+                  )),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  Flexible(child: TextField(
+                    controller: _phoneController,
+                    decoration: const InputDecoration(hintText: 'Phone'),
+                  ))
+                ],
               ),
               const SizedBox(
                 height: 10,
               ),
-              TextField(
-                controller: _addressController,
-                decoration: const InputDecoration(hintText: 'Address'),
+              Row(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Flexible(child: TextField(
+                    controller: _modelController,
+                    decoration: const InputDecoration(hintText: 'Model'),
+                  )),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  Flexible(child: TextField(
+                    controller: _fluelController,
+                    decoration: const InputDecoration(hintText: 'Fluel'),
+                  ))
+                ],
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Row(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Flexible(child: TextField(
+                    controller: _numberController,
+                    decoration: const InputDecoration(hintText: 'Number'),
+                  )),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  Flexible(child: TextField(
+                    controller: _registeredCodeController,
+                    decoration: const InputDecoration(hintText: 'RegisteredCode'),
+                  ))
+                ],
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Row(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Flexible(child: TextField(
+                    controller: _lastMarkController,
+                    decoration: const InputDecoration(
+                      icon: Icon(Icons.calendar_today),
+                      hintText: 'Last Mark'
+                    ),
+                    readOnly: true,
+                    onTap: () async {
+                      _lastMark = await showDatePicker(
+                          context: context,
+                          initialDate: DateTime.now(),
+                          firstDate: DateTime(2000),
+                          lastDate: DateTime(2060));
+                      if (_lastMark != null) {
+                        setState(() {
+                          _lastMarkController.text = DateFormat('dd/MM/yyyy').format(_lastMark!);
+                        });
+                      }
+                    },
+                  )),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  Flexible(child: TextField(
+                    controller: _lastDeadlineController,
+                    decoration: const InputDecoration(
+                        icon: Icon(Icons.calendar_today),
+                        hintText: 'Last Deadline'
+                    ),
+                    readOnly: true,
+                    onTap: () async {
+                      _lastDeadline = await showDatePicker(
+                          context: context,
+                          initialDate: DateTime.now(),
+                          firstDate: DateTime(2000),
+                          lastDate: DateTime(2060));
+                      if (_lastDeadline != null) {
+                        setState(() {
+                          _lastDeadlineController.text = DateFormat('dd/MM/yyyy').format(_lastDeadline!);
+                        });
+                      }
+                    },
+                  )),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  Flexible(child: TextField(
+                    controller: _deadlineController,
+                    decoration: const InputDecoration(
+                        icon: Icon(Icons.calendar_today),
+                        hintText: 'Next Deadline'
+                    ),
+                    readOnly: true,
+                    onTap: () async {
+                      _deadline = await showDatePicker(
+                          context: context,
+                          initialDate: DateTime.now(),
+                          firstDate: DateTime(2000),
+                          lastDate: DateTime(2060));
+                      if (_deadline != null) {
+                        setState(() {
+                          _deadlineController.text = DateFormat('dd/MM/yyyy').format(_deadline!);
+                        });
+                      }
+                    },
+                  )),
+                ],
               ),
               const SizedBox(
                 height: 20,
@@ -85,6 +242,17 @@ class _HomePageState extends State<HomePage> {
                   _firstnameController.text = '';
                   _lastnameController.text = '';
                   _addressController.text = '';
+                  _phoneController.text = '';
+                  _modelController.text = '';
+                  _fluelController.text = '';
+                  _numberController.text = '';
+                  _registeredCodeController.text = '';
+                  _lastDeadlineController.text = '';
+                  _lastMarkController.text = '';
+                  _deadlineController.text = '';
+                  _lastDeadline = null;
+                  _lastMark = null;
+                  _deadline = null;
 
                   // Close the bottom sheet
                   Navigator.of(context).pop();
@@ -96,19 +264,20 @@ class _HomePageState extends State<HomePage> {
         ));
   }
 
-// Insert a new journal to the database
   Future<void> _addItem() async {
-    await _customerRepository.createCustomer(_firstnameController.text,
-        _lastnameController.text, _addressController.text, null, null);
+    final Customer customer = await _customerRepository.createCustomer(_firstnameController.text,
+        _lastnameController.text, _addressController.text, _phoneController.text);
+    await _machineRepository.createMachine(customer.id, _modelController.text,
+        _fluelController.text, _numberController.text,
+        _registeredCodeController.text, _lastMark, _lastDeadline, _deadline
+    );
   }
 
-  // Update an existing journal
   Future<void> _updateItem(String id) async {
     await _customerRepository.updateCustomer(id, _firstnameController.text,
         _lastnameController.text, _addressController.text, null);
   }
 
-  // Delete an item
   void _deleteItem(String id) async {
     await _customerRepository.deleteCustomer(id);
     ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
@@ -124,7 +293,8 @@ class _HomePageState extends State<HomePage> {
         title: const Text('Gestio'),
       ),
       body: SafeArea(
-        child: Center(
+        child: Container(
+          width: double.infinity,
           child: StreamBuilder(
               stream: _customerRepository.allCustomerStream(),
               builder: (context, snapshot) {
