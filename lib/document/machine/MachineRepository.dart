e8ad4ef3a5edc90fd5aa5fe019044faa9fc61deb
Mario Ravalli <mario.ravalli@bakeca.it>
2022-10-25 09:45:05 +0200

    Added method for calculate deadline if not provided

--- lib/document/machine/MachineRepository.dart
+++ lib/document/machine/MachineRepository.dart
@@ -3,8 +3,36 @@ import 'package:cbl/cbl.dart';
 import 'Machine.dart';
 
 class MachineRepository {
+  static const yearMark = 4;
+  static const yearDeadline = 2;
+
   final Database _database;
   MachineRepository(this._database);
+
+  DateTime _deadline(
+      DateTime? deadline,
+      DateTime? lastMark,
+      DateTime? lastDeadline) {
+    if (deadline != null) {
+      return deadline;
+    }
+    var _lastMark = lastMark?.add(const Duration(days: 365 * yearMark));
+    var _lastDeadline = lastDeadline?.add(const Duration(days: 365 * yearDeadline));
+    if (_lastMark != null && _lastDeadline != null) {
+      if (_lastMark.isAfter(_lastDeadline)) {
+        return _lastDeadline;
+      } else {
+        return _lastMark;
+      }
+    }
+    if (_lastDeadline != null) {
+      return _lastDeadline;
+    }
+    if (_lastMark != null) {
+      return _lastMark;
+    }
+    return DateTime.now();
+  }
 
   Future<Machine> createMachine(
       String owner, String model, String fluel, String? number,
@@ -21,7 +49,7 @@ class MachineRepository {
       'registeredCode': registeredCode,
       'lastMark': lastMark,
       'lastDeadline': lastDeadline,
-      'deadline': deadline,
+      'deadline': _deadline(deadline, lastMark, lastDeadline),
     });
     await _database.saveDocument(document);
     return Machine(document);
@@ -40,7 +68,7 @@ class MachineRepository {
     mutableDoc.setValue(registeredCode, key: 'registeredCode');
     mutableDoc.setValue(lastMark, key: 'lastMark');
     mutableDoc.setValue(lastDeadline, key: 'lastDeadline');
-    mutableDoc.setValue(deadline, key: 'deadline');
+    mutableDoc.setValue(_deadline(deadline, lastMark, lastDeadline), key: 'deadline');
     return await _database.saveDocument(mutableDoc);
   }
 }
