e6db64514b146bea81401d44c3c05e85bb5a4ad0
Mario Ravalli <mario.ravalli@bakeca.it>
2022-10-26 22:29:57 +0200

    Added license

--- lib/domain/machine/MachineAbstract.dart
+++ lib/domain/machine/MachineAbstract.dart
@@ -1,3 +1,22 @@
+/*
+ * This file is part of Gestio.
+ *
+ * Copyright (c) 2022 Mario Ravalli.
+ *
+ * Gestio is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * Gestio is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Gestio. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 abstract class MachineAbstract {
   String get id;
   DateTime get createdAt;
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,27 @@
+/*
+ * This file is part of Gestio.
+ *
+ * Copyright (c) 2022 Mario Ravalli.
+ *
+ * Gestio is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * Gestio is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Gestio. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:gestio/infrastructure/services/db/DatabaseHelper.dart';
-import 'package:gestio/presentation/screens/engagement/Home.dart';
+import 'package:gestio/presentation/screens/engagement/Engagement.dart';
 
 Future<void> main() async {
   await initApp();
@@ -38,6 +57,6 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.orange,
         ),
-        home: HomePage());
+        home: const EngagementScreen());
   }
 }
